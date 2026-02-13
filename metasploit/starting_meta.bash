82384a76c9b303dd0fa6d530fe72111989a600d9
Deepak <102180983+karkideepak@users.noreply.github.com>
2026-02-12 22:50:56 -0600

    Create starting_meta.bash

---
+++ metasploit/starting_meta.bash
@@ -0,0 +1,6 @@
+# Most common way (Kali, Parrot, BlackArch...)
+sudo msfdb init    # only needed first time or after big updates
+msfconsole         # normal start (with cool banner)
+
+# Faster / cleaner start (recommended for beginners)
+msfconsole -q      # -q = quiet (no banner)
